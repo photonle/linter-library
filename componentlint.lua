cc77099b7a4c71750bd7ebe4818b6adc11e81014
John Internet <jonjon1234.github@gmail.com>
2019-11-03 19:19:29 +0000

    And fix the output.

--- componentlint.lua
+++ componentlint.lua
@@ -52,8 +52,45 @@ function EMVU:AddAutoComponent(data, name)
             end
         end
     end
+
+    runner:Test()
+    table.insert(self.stored, runner)
 end
 
 for _, file in ipairs(files) do
     dofile(file)
 end
+
+local failed = {}
+local succeeded = 0
+local asserts = 0
+for _, runner in ipairs(EMVU.stored) do
+    for _, test in ipairs(runner.tests) do
+        if test.success then
+            succeeded = succeeded + 1
+            asserts = asserts + test.assertions
+        else
+            print(test)
+            table.insert(failed, string.format(
+                    "Test %s :: [%s][%s]\n\t%s",
+                    test.errored and "Errored" or "Failed",
+                    runner.name,
+                    test.name,
+                    test.errored and test.error or test.message
+            ))
+        end
+    end
+end
+
+local count = #failed
+local tests = succeeded + #failed
+local sirens = #EMVU.stored
+print(string.format(
+        "Stats:\n\tComponents Checked: %d\n\tTests Ran: %d\n\tTests Succeeded: %d\n\tTests Failed: %d\n\tAssertions: %d",
+        sirens, tests, succeeded, count, asserts
+))
+for _, msg in ipairs(failed) do
+    print(msg)
+end
+
+os.exit(math.min(count, 1))
